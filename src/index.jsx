c6491103093a4d94218509eb2b37682f0210d4ff
Asai Toshiya <to.asai.60@gmail.com>
2022-07-30 08:06:39 +0900

    ダーク モード オプションを追加する
    #2

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// テーマ
+const theme = new URLSearchParams(window.location.search).get("theme");
+const isDark = theme === "dark";
+document.documentElement.style.colorScheme = isDark ? "dark" : "light";
+const head = document.getElementsByTagName("head")[0];
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = isDark ? "/antd.dark.min.css" : "/antd.min.css";
+head.appendChild(link);
+
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
